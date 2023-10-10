87992841a788dae440ed65f65ab619b05b13c5be
Jason Torres <126519853+C0m91ex@users.noreply.github.com>
2023-10-09 17:37:43 -0700

    Add files via upload
    Adding coding journal Prompt #3

---
+++ CodeJournalPrompt#3.py
@@ -0,0 +1,18 @@
+# Coding Journal Prompt #3
+
+# Define the function f(x)
+def f(x):
+    return x*3 + 8
+
+# Main function
+def main():
+    x = 9
+    result = f(x)
+    print("Result:", result)
+
+    if result > 27:
+        print("YAY!")
+
+# Call the main function
+if __name__ == "__main__":
+    main()
