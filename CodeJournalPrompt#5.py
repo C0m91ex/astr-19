00a9ccf0b603dbdc7ce7046d8e1365c7a347afe1
Jason Torres <126519853+C0m91ex@users.noreply.github.com>
2023-10-17 15:14:19 -0700

    Add files via upload

---
+++ CodeJournalPrompt#5.py
@@ -0,0 +1,29 @@
+# Coding Journal Prompt #5
+
+import numpy as np 
+
+# This function will generate the sin values for the table
+def generate_sin_table(start, end, number_points):
+	x = np.linspace(start, end, number_points)
+	sin_x = np.sin(x)
+	table = list(zip(x, sin_x))
+	return table 
+
+# This function will print out the table
+def print_sin_table(table):
+	print("x\t| sin(x)")
+	print("-" * 15)
+	for x, sin_x in table:
+		print(f"{x:.4f}\t| {sin_x:.4f}")
+
+# This is the main calling function
+def main():
+	start = 0
+	end = 2 * np.pi 
+	number_points = 10
+
+	sin_table = generate_sin_table(start, end, number_points)
+	print_sin_table(sin_table)
+
+if __name__ == "__main__":
+	main()
